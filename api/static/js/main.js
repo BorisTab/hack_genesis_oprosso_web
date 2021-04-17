4a97203c2b01ba31a363ca5b8dc9ad7df2b20250
BorisTab <boristab23@gmail.com>
2021-04-17 23:49:32 +0300

    fix back

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -1,48 +1,100 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var modeSwitch = document.querySelector('.mode-switch');
+  var modeSwitch = document.querySelector('.mode-switch');
+
+  modeSwitch.addEventListener('click', function () {                     
+      document.documentElement.classList.toggle('dark');
+      modeSwitch.classList.toggle('active');
+  });
   
-    modeSwitch.addEventListener('click', function () {                     
-        document.documentElement.classList.toggle('dark');
-        modeSwitch.classList.toggle('active');
-    });
-    
-    var listView = document.querySelector('.list-view');
-    var gridView = document.querySelector('.grid-view');
-    var projectsList = document.querySelector('.project-boxes');
-    
-    listView.addEventListener('click', function () {
-      gridView.classList.remove('active');
-      listView.classList.add('active');
-      projectsList.classList.remove('jsGridView');
-      projectsList.classList.add('jsListView');
-    });
-    
-    gridView.addEventListener('click', function () {
-      gridView.classList.add('active');
-      listView.classList.remove('active');
-      projectsList.classList.remove('jsListView');
-      projectsList.classList.add('jsGridView');
-    });
-    
-    document.querySelector('.messages-btn').addEventListener('click', function () {
-      document.querySelector('.messages-section').classList.add('show');
-    });
+  var listView = document.querySelector('.list-view');
+  var gridView = document.querySelector('.grid-view');
+  var projectsList = document.querySelector('.project-boxes');
+  
+  listView.addEventListener('click', function () {
+    gridView.classList.remove('active');
+    listView.classList.add('active');
+    projectsList.classList.remove('jsGridView');
+    projectsList.classList.add('jsListView');
+  });
+  
+  gridView.addEventListener('click', function () {
+    gridView.classList.add('active');
+    listView.classList.remove('active');
+    projectsList.classList.remove('jsListView');
+    projectsList.classList.add('jsGridView');
+  });
+  
+  document.querySelector('.messages-btn').addEventListener('click', function () {
+    document.querySelector('.messages-section').classList.add('show');
+  });
 
-    /*Показ модального окна*/
-    $('.add-btn').on('click',
-            function () {
-                $('.overlay,.modal').show();
-            }
-    );
-    /*Закрытие модального окна*/
-    $('.overlay,.close').on('click',function(){
-        $('.overlay,.modal').hide();
-    });
+  /*Показ модального окна*/
+  $('.add-btn').on('click',
+          function () {
+              $('.overlay,.modal').show();
+          }
+  );
+  /*Закрытие модального окна*/
+  $('.submit-button,.overlay,.close').on('click',function(){
+      $('.overlay,.modal').hide();
+  });
 
-    $('input[type="file"]').change(function(){      
-      var fileName = $(this).val().split('/').pop().split('\\').pop();
-      // console.log(fileName);
-      $('.input-file-name-info').text(fileName);
+  $('input[type="file"]').change(function(){      
+    var fileName = $(this).val().split('/').pop().split('\\').pop();
+    // console.log(fileName);
+    $('.input-file-name-info').text(fileName);
+  });
 
+  $('.card-3-button').on('click',
+    function () {
+      $('.overlay,.card-info').show();
+    }
+  );
+
+  $('.overlay,.close').on('click',function(){
+    $('.overlay,.card-info').hide();
+  });
+
+  $('.submit-button').on('click', () => {
+    let form = {
+      name: $('.task-name-input').val(),
+      description: $('.task-description-input-field').val(),
+      num_of_people: parseInt($('.people-num-input').val(), 10),
+      file_ref: $('.file-ref-input').val()
+    };
+
+
+    console.log(form);
+
+    fetch('/api/add/', {
+      method: 'post',
+      body: JSON.stringify(form)
     });
   });
+
+});
+
+fetch('/api/7/')
+.then((response) => {
+  if (response.status !== 200) {
+    console.log("Ничего. Status: " + response.status);
+    return;
+  }
+
+  response.json().then((data) => {
+    console.log(data);
+
+    let perc = data.cur_people / data.num_of_people * 100;
+    console.log(perc);
+
+    $('.card-3-date').text(data.date);
+    $('.card-3-name').text(data.name);
+    $('.task-name').text(data.name);
+    $('.task-description').html(data.description.replace(/\n/g, "<br />"));
+    $('.card-3-progress').text(perc + '%');
+    $('.card-3-box-progress').css("width", perc + '%');
+  });
+})
+.catch((error) => {
+  console.log("Fetch error: -S", error);
+});
--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from androguard.misc import AnalyzeAPK
 from django.utils import timezone
-from .forms import ModelFormWithFileField
+# from .forms import ModelFormWithFileField
 from .models import Task
 
 def download_apk(url, output):
@@ -23,14 +23,14 @@ def get_package_name(filename):
 def decode_json(request):
     return json.loads(request.body.decode('utf-8'))
 
-def convert_json(id, name, description, num_people, cur_people, link, package_name, pub_date):
-    inf_task = {'task_id' : id, 'name' : name, 'description' : description, 'num_of_people' : num_people, 'cur_people' : cur_people,'file_ref' : link, 'pkg_name' : package_name, 'date' : pub_date}
+def convert_json(id, name, description, need_people, cur_people, link, package_name, pub_date):
+    inf_task = {'task_id' : id, 'name' : name, 'description' : description, 'num_of_people' : need_people, 'cur_people' : cur_people,'file_ref' : link, 'pkg_name' : package_name, 'date' : pub_date}
     return JsonResponse(inf_task)
 
 @csrf_exempt
 def add_task(request):
     new_task = decode_json(request)
-    download_apk(new_task.link, 'app.apk')
+    download_apk(new_task['file_ref'], 'app.apk')
     package_name = get_package_name('app.apk')
     new = Task(name=new_task['name'], description=new_task['description'], need_people=new_task['num_of_people'], cur_people=new_task['cur_people'], link=new_task['file_ref'], pkg_name=package_name, pub_date=datetime.datetime.now().strftime('%d %B, %Y'))
     new.save()
@@ -52,15 +52,3 @@ def show_task(request, task_id):
 
 def render_page(request):
     return render(request, 'frontend/index.html')
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = ModelFormWithFileField(request.POST, request.FILES)
-        if form.is_valid():
-            # file is saved
-            form.save()
-            return HttpResponseRedirect('/success/url/')
-    else:
-        form = ModelFormWithFileField()
-    return render(request, 'upload.html', {'form': form})
-
