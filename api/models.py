d1c0867a624e91fb6633f250127e539d26147ba4
KristinaKulabuhova <kulabukhova.ka@phystech.edu>
2021-04-17 20:50:18 +0300

    add fields in Task

--- api/models.py
+++ api/models.py
@@ -6,10 +6,12 @@ from django.utils import timezone
 class Task(models.Model):
     task_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    people = models.IntegerField(default=0)
+    description = models.CharField(max_length=1000)
+    need_people = models.IntegerField(default=0)
+    cur_people = models.IntegerField(default=0)
     link = models.CharField(max_length=200)
-    pub_date = models.DateField()
+    pkg_name = models.CharField(max_length=200)
+    pub_date = models.CharField(max_length=200)
 
 class RunTask(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
--- api/views.py
+++ api/views.py
@@ -10,14 +10,15 @@ from .models import Task
 def decode_json(request):
     return json.loads(request.body.decode('utf-8'))
 
-def convert_json(id, name, description, people, link, pub_date):
-    inf_task = {'task_id' : id, 'name' : name, 'description' : description, 'num_of_people' : people, 'file_ref' : link, 'date' : pub_date}
+def convert_json(id, name, description, num_people, cur_people, link, package_name, pub_date):
+    inf_task = {'task_id' : id, 'name' : name, 'description' : description, 'num_of_people' : num_people, 'cur_people' : cur_people,'file_ref' : link, 'pkg_name' : package_name, 'date' : pub_date}
     return JsonResponse(inf_task)
 
 @csrf_exempt
 def add_task(request):
     new_task = decode_json(request)
-    new = Task(name=new_task['name'], description=new_task['description'], people=new_task['num_of_people'], link=new_task['file_ref'], pub_date=timezone.now())
+    # function for package_name
+    new = Task(name=new_task['name'], description=new_task['description'], need_people=new_task['num_of_people'], cur_people=new_task['cur_people'], link=new_task['file_ref'], pkg_name='package_name', pub_date=datetime.datetime.now().strftime('%d %A %Y'))
     new.save()
     return HttpResponse(200)
 
@@ -26,15 +27,14 @@ def show_all_tasks(request):
     all_tasks_json = []
 
     for task in all_tasks:
-        all_tasks_json.append(convert_json(task.task_id, task.name, task.description, task.people, task.link, task.pub_date))
+        all_tasks_json.append(convert_json(task.task_id, task.name, task.description, task.need_people, task.cur_people, task.link, task.pkg_name, task.pub_date))
         
     JsonResponse(all_tasks_json)
   
 
 def show_task(request, task_id):
-    # id_of_task = decode_json(request)
     task = Task.objects.get(task_id=task_id)
-    inf_task = convert_json(str(task.task_id), task.name, task.description, str(task.people), task.link, str(task.pub_date))
+    inf_task = convert_json(str(task.task_id), task.name, task.description, str(task.need_people), str(task.cur_people), task.link, task.pkg_name, str(task.pub_date))
     return inf_task
 
 def render_page(request):
